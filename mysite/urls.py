0ddb1df1a6f4eb45ed566549da841e49dcefa96f
zhanglianshen <13621741600@163.com>
2020-01-18 19:54:09 +0800

    第十次同步文件

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
-from .views import home
+from .views import home,login
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -27,6 +27,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('login/',login,name='login'),
+    path('comment/', include('comment.urls')),
+    
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- mysite/views.py
+++ mysite/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect,reverse
+from django.contrib import auth
 
 def home(request):
     context={}
     return render(request,'home.html',context)
+
+def login(request):
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+    referer=request.META.get('HTTP_REFERER',reverse('home'))
+    user = auth.authenticate(request, username=username, password=password)
+    if user is not None:
+        auth.login(request, user)
+        return redirect(referer)
+    else:
+        return render(request, 'error.html', {'message':'用户名或密码不正确'})
